ecb5d6f089155ba305f10c23796abe38c5f5cf57
Elena <elenamalinina2010@yandex.ru>
2021-09-23 12:28:05 +0300

    Домашнее задание 5

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -2,6 +2,8 @@ package lesson5;
 
 import java.io.IOException;
 import java.util.Arrays;
+//1. Реализовать сохранение данных в csv файл;
+//2. Реализовать загрузку данных из csv файла. Файл читается целиком.
 
 public class  Main {
     public static void main(String[] args) throws IOException {
